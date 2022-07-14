1a19a1ff4a78dfceade046c09fa8bba9563e3a1e
mikewii <mike.sivkov@gmail.com>
2022-07-14 15:32:47 +0300

    Sort: visual bug fix

--- include/Tools/Sort.hpp
+++ include/Tools/Sort.hpp
@@ -7,7 +7,7 @@ void sort_swap_only(T begin, T end, const Comparator comp)
     if (begin == end)
         return;
 
-    for (auto& left = begin; begin != end; left++)
+    for (auto& left = begin; left != end; left++)
         for (auto right = left; right != end; ++right)
             if (comp(*left, *right))
                 swap(*left, *right);
